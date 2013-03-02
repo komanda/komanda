e9f71f3c3103b0eec17cbaa48daabecd07d31e10
= <=>
2013-03-01 20:47:05 -0800

    Mail fix...

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,23 +1,23 @@
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => "www.komanda.us",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
-# ActionMailer::Base.delivery_method = :smtp
-
 # ActionMailer::Base.smtp_settings = {
-#   :address              => "smtp.gmail.com",
-#   :port                 => 587,
-#   :domain               => "gmail.com",
-#   :user_name            => "komandalife",
-#   :password             => "komanda02062012",
-#   :authentication       => "plain",
+#   :user_name => ENV['SENDGRID_USERNAME'],
+#   :password => ENV['SENDGRID_PASSWORD'],
+#   :domain => "www.komanda.us",
+#   :address => "smtp.sendgrid.net",
+#   :port => 587,
+#   :authentication => :plain,
 #   :enable_starttls_auto => true
 # }
+# ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => "komandalife",
+  :password             => "komanda02062012",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
 # 
 # if Rails.env.development?
 #   ActionMailer::Base.default_url_options[:host] = "localhost:3000"
