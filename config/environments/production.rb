33ccb8d259c6fd781c83eb8ac6340b7f57400a38
= <=>
2013-03-01 18:19:36 -0800

    Mail changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Komanda::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
   
   # PayPal config
   config.after_initialize do
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,11 @@
 ActionMailer::Base.smtp_settings = {
-  :port           => 25, 
-  :address        => "smtp.sendgrid.net",
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'komana.herokuapp.com'
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],,
+  :domain => "www.komanda.us",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
 }
 # ActionMailer::Base.delivery_method = :smtp
 
